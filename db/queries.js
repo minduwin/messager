e99528d5cde94b5605c21a0556b513a410a8ee8b
minduwin <88805397+minduwin@users.noreply.github.com>
2026-03-13 16:09:20 -0400

    Fix bug in queries.js

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require('./pool');
 
 async function getAllMessages() {
-    const { rows } = await pool.query('SELECT * FROM usernames');
+    const { rows } = await pool.query('SELECT * FROM messages');
     return rows;
 }
 
